c63584c6b943b4d2d5a5c7e2ce6e698361315934
stwa <s68772@beuth-hochschule.de>
2018-09-14 19:18:02 +0200

    update tonegenerator and clean up src-code

--- app/src/main/java/com/example/steffen/theremin/MainActivity.java
+++ app/src/main/java/com/example/steffen/theremin/MainActivity.java
@@ -1,18 +1,12 @@
 package com.example.steffen.theremin;
 
-import android.app.Activity;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
-import android.media.ToneGenerator;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -23,369 +17,14 @@ public class MainActivity extends AppCompatActivity {
     ImageButton btPause;
     Button btPlus;
     Button btMinus;
-    ToneGenerator toneGenerator;
 
-    // aktuell gespielter Ton
     TextView tone;
 
+    int currentScaleIndex = 0;
 
-    // Frequenzen der Töne
-    String C = "262";
-    String Cis = "277";
-    String D = "294";
-    String Dis = "311";
-    String E = "330";
-    String F = "349";
-    String Fis = "370";
-    String G = "392";
-    String Gis = "415";
-    String A = "440";
-    String Ais = "466";
-    String B = "494";
-    String C2 = "523";
-    String Cis2 = "554";
-    String D2 = "587";
-    String Dis2 = "622";
-    String E2 = "659";
-    String F2 = "698";
-    String Fis2 = "740";
-    String G2 = "784";
-    String Gis2 = "831";
-    String A2 = "880";
-    String Ais2 = "932";
-    String B2 = "988";
+    ToneGeneratorHelper toneGeneratorHelper = new ToneGeneratorHelper();
+    com.example.steffen.theremin.ToneGenerator toneGenerator = new com.example.steffen.theremin.ToneGenerator();
 
-
-    //===TONLEITERNN===
-/*    String blabla[] = {
-            "C-Dur", "G-Dur", "D-Dur", "A-Dur", "E-Dur", "B-Dur", "F#-Dur", "F-Dur", "Bb-Dur", "Eb-Dur", "Ab-Dur", "Db-Dur", "Gb-Dur", "A-Moll", "E-Moll", "B-Moll", "F#-Moll", "C#-Moll", "G#-Moll", "D#-Moll", "D-Moll", "G-Moll", "C-Moll", "F-Moll", "Bb-Moll", "Eb-Moll"
-    };*/
-
-    // C-Dur
-    String cDur[][] = {
-            {C, "C"},
-            {D, "D"},
-            {E, "E"},
-            {F, "F"},
-            {G, "G"},
-            {A, "A"},
-            {B, "B"},
-            {C2, "C"}
-    };
-
-    // A-Moll
-    String aMoll[][] = {
-            {A, "A"},
-            {B, "B"},
-            {C2, "C"},
-            {D2, "D"},
-            {E2, "E"},
-            {F2, "F"},
-            {G2, "G"},
-            {A2, "A"}
-    };
-
-    //#-Tonarten
-    // G-Dur
-    String gDur[][] = {
-            {G, "G"},
-            {A, "A"},
-            {B, "B"},
-            {C2, "C"},
-            {D2, "D"},
-            {E2, "E"},
-            {Fis2, "F#"},
-            {G2, "G"}
-    };
-
-    // E-Moll
-    String eMoll[][] = {
-            {E, "E"},
-            {Fis, "F#"},
-            {G, "G"},
-            {A, "A"},
-            {B, "B"},
-            {C2, "C"},
-            {D2, "D"},
-            {E2, "E"}
-    };
-
-    // D-Dur
-    String dDur[][] = {
-            {D, "D"},
-            {E, "E"},
-            {Fis, "F#"},
-            {G, "G"},
-            {A, "A"},
-            {B, "B"},
-            {Cis2, "C#"},
-            {D2, "D"}
-    };
-
-    // B-Moll
-    String bMoll[][] = {
-            {B, "B"},
-            {Cis2, "C#"},
-            {D2, "D"},
-            {E2, "E"},
-            {Fis2, "F#"},
-            {G2, "G"},
-            {A2, "A"},
-            {B2, "B"}
-    };
-
-    // A-Dur
-    String aDur[][] = {
-            {A, "A"},
-            {B, "B"},
-            {Cis2, "C#"},
-            {D2, "D"},
-            {E2, "E"},
-            {Fis2, "F#"},
-            {Gis2, "G#"},
-            {A2, "A"}
-    };
-
-    // F#-Moll
-    String fisMoll[][] = {
-            {Fis, "F#"},
-            {Gis, "G#"},
-            {A, "A"},
-            {B, "B"},
-            {Cis2, "C#"},
-            {D2, "D"},
-            {E2, "E"},
-            {Fis2, "F#"}
-    };
-
-    // E-Dur
-    String eDur[][] = {
-            {E, "E"},
-            {Fis, "F#"},
-            {Gis, "G#"},
-            {A, "A"},
-            {B, "B"},
-            {Cis2, "C#"},
-            {Dis2, "D#"},
-            {E2, "E"}
-    };
-
-    // C#-Moll
-    String cisMoll[][] = {
-            {Cis, "C#"},
-            {Dis, "D#"},
-            {E, "E"},
-            {Fis, "F#"},
-            {Gis, "G#"},
-            {A, "A"},
-            {B, "B"},
-            {Cis2, "C#"}
-    };
-
-    // B-Dur
-    String bDur[][] = {
-            {B, "B"},
-            {Cis2, "C#"},
-            {Dis2, "D#"},
-            {E2, "E"},
-            {Fis2, "F#"},
-            {Gis2, "G#"},
-            {Ais2, "A#"},
-            {B2, "B"}
-    };
-
-    // G#-Moll
-    String gisMoll[][] = {
-            {Gis, "G#"},
-            {Ais, "A#"},
-            {B, "B"},
-            {Cis2, "C#"},
-            {Dis2, "D#"},
-            {E2, "E"},
-            {Fis2, "F#"},
-            {Gis2, "G#"}
-    };
-
-    // F#-Dur
-    String fisDur[][] = {
-            {Fis, "G#"},
-            {Gis, "A#"},
-            {Ais, "B"},
-            {B, "C#"},
-            {Cis2, "D#"},
-            {Dis2, "E"},
-            {F2, "E#"}, //enharmonische Verwechslung
-            {Fis2, "G#"}
-    };
-
-    // D#-Moll
-    String disMoll[][] = {
-            {Dis, "D#"},
-            {F, "E#"}, //enharmonische Verwechslung
-            {Fis, "F#"},
-            {Gis, "G#"},
-            {Ais, "A#"},
-            {B, "B"},
-            {Cis2, "C#"},
-            {Dis2, "D#"}
-    };
-
-    // b-Tonarten
-    // F-Dur
-    String fDur[][] = {
-            {F, "F"},
-            {G, "G"},
-            {A, "A"},
-            {Ais, "Bb"},
-            {C2, "C"},
-            {D2, "D"},
-            {E2, "E"},
-            {F2, "F"}
-    };
-
-    // D-Moll
-    String dMoll[][] = {
-            {D, "D"},
-            {E, "E"},
-            {F, "F"},
-            {G, "G"},
-            {A, "A"},
-            {Ais, "Bb"},
-            {C2, "C"},
-            {D2, "D"}
-    };
-
-    // Bb-Dur
-    String bbDur[][] = {
-            {Ais, "Bb"},
-            {C2, "C"},
-            {D2, "D"},
-            {Dis2, "Eb"},
-            {F2, "F"},
-            {G2, "G"},
-            {A2, "A"},
-            {Ais2, "Bb"}
-    };
-
-    // G-Moll
-    String gMoll[][] = {
-            {G, "G"},
-            {A, "A"},
-            {Ais, "Bb"},
-            {C2, "C"},
-            {D2, "D"},
-            {Dis2, "Eb"},
-            {F2, "F"},
-            {G2, "G"}
-    };
-
-    // Eb-Dur
-    String ebDur[][] = {
-            {Dis, "Eb"},
-            {F, "F"},
-            {G, "G"},
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {C2, "C"},
-            {D2, "D"},
-            {Dis2, "Eb"}
-    };
-
-    // C-Moll
-    String cMoll[][] = {
-            {C, "C"},
-            {D, "D"},
-            {Dis, "Eb"},
-            {F, "F"},
-            {G, "G"},
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {C2, "C"}
-    };
-
-    // Ab-Dur
-    String abDur[][] = {
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {C2, "C"},
-            {Cis2, "Db"},
-            {Dis2, "Eb"},
-            {F2, "F"},
-            {G2, "G"},
-            {Gis2, "Ab"}
-    };
-
-    // F-Moll
-    String fMoll[][] = {
-            {F, "F"},
-            {G, "G"},
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {C2, "C"},
-            {Cis2, "Db"},
-            {Dis2, "Eb"},
-            {F2, "F"}
-    };
-
-    // Db-Dur
-    String dbDur[][] = {
-            {Cis, "Db"},
-            {Dis, "Eb"},
-            {F, "F"},
-            {Fis, "Gb"},
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {C, "C"},
-            {Cis2, "Db"}
-    };
-
-    // Bb-Moll
-    String bbMoll[][] = {
-            {Ais, "Bb"},
-            {C2, "C"},
-            {Cis2, "Db"},
-            {Dis2, "Eb"},
-            {F2, "F"},
-            {Fis2, "Gb"},
-            {Gis2, "Ab"},
-            {Ais2, "Bb"}
-    };
-
-    // Gb-Dur
-    String gbDur[][] = {
-            {Fis, "Gb"},
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {B, "Cb"}, //enharmonische Verwechslung
-            {Cis2, "Db"},
-            {Dis2, "Eb"},
-            {F2, "F"},
-            {Fis2, "Gb"}
-    };
-
-    // Eb-Moll
-    String ebMoll[][] = {
-            {Dis, "Eb"},
-            {F, "F"},
-            {Fis, "Gb"},
-            {Gis, "Ab"},
-            {Ais, "Bb"},
-            {B, "Cb"}, //enharmonische Verwechslung
-            {Cis2, "Db"},
-            {Dis2, "Eb"}
-    };
-
-    // ===TONLEITERN ENDE===
-
-    // Tonleiter setzen
-    String scale[][] = cDur;
-
-    int duration = 1;
-    int frequency = 440;
-    int currentIndex = 0;
-
-    int sampleRate = 44100; // Feel free to change this
-    byte soundData[] = new byte[sampleRate * duration];
 
 
     @Override
@@ -398,23 +37,37 @@ public class MainActivity extends AppCompatActivity {
         btMinus = (Button) findViewById(R.id.minusButton);
         tone = (TextView) findViewById(R.id.currentTone);
 
-        // Tonleiter mit Spinner auswählen (funktioniert noch nicht)
-        // https://developer.android.com/guide/topics/ui/controls/spinner
         Spinner keys = (Spinner) findViewById(R.id.keysSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.keys_array, android.R.layout.simple_spinner_dropdown_item);
         keys.setAdapter(adapter);
+        keys.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String spinnerItem=parent.getItemAtPosition(position).toString();
+                Log.i("Spinner", "onItemSelected: " + spinnerItem);
 
+                toneGeneratorHelper.setScale((String[][]) toneGeneratorHelper.getScaleMap().get(spinnerItem));
+                currentScaleIndex = 0;
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
 
         btPlay.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+
                 btPlay.setVisibility(View.GONE);
                 btPause.setVisibility(View.VISIBLE);
-                generateSound(Integer.parseInt(scale[currentIndex][0]));
-                Log.d("Key", "Name: " + scale[0][1] + " Frequenz: " + scale[0][0]);
+                toneGenerator.m_ifreq = Integer.parseInt(toneGeneratorHelper.getScale()[currentScaleIndex][0]);
+                toneGenerator.sampleRate = toneGeneratorHelper.getSampleRate();
+                toneGenerator.play();
+                showTone();
+                Log.i("Key", "Name: " + toneGeneratorHelper.getScale()[currentScaleIndex][1] + " Frequenz: " + toneGeneratorHelper.getScale()[currentScaleIndex][0]);
             }
         });
 
@@ -425,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 btPause.setVisibility(View.GONE);
                 btPlay.setVisibility(View.VISIBLE);
-                stopSound();
+                toneGenerator.stop();
 
             }
         });
@@ -434,13 +87,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                if(currentIndex < scale.length - 1){
-                currentIndex = currentIndex + 1;
-                stopSound();
-                generateSound(Integer.parseInt(scale[currentIndex][0]));
+                if( currentScaleIndex < toneGeneratorHelper.getScale().length - 1){
+                    currentScaleIndex++;
+                    toneGenerator.stop();
+                    toneGenerator.m_ifreq = Integer.parseInt(toneGeneratorHelper.getScale()[currentScaleIndex][0]);
+                    toneGenerator.play();
+                    showTone();
                 }
-
-
             }
         });
 
@@ -449,71 +102,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                if(currentIndex > 0){
-                    currentIndex = currentIndex - 1;
-                    stopSound();
-                    generateSound(Integer.parseInt(scale[currentIndex][0]));
+                if( currentScaleIndex > 0){
+                    currentScaleIndex--;
+                    toneGenerator.stop();
+                    toneGenerator.m_ifreq = Integer.parseInt(toneGeneratorHelper.getScale()[currentScaleIndex][0]);
+                    toneGenerator.play();
+                    showTone();
                 }
-
-
             }
         });
-
-        toneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 50);
-    }
-
-    public void generateSound(int frequency){
-        Log.d("Button", "Playbutton clicked");
-        Log.d("Index", String.valueOf(currentIndex));
-
-
-        for(int i = 0; i < soundData.length; i++) {
-            byte sample = (byte)(
-                    Math.sin(2 * Math.PI * frequency * i / sampleRate) *
-                            255);
-            soundData[i] = sample;
-        }
-
-
-
-        AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate,
-                AudioFormat.CHANNEL_OUT_DEFAULT,
-                AudioFormat.ENCODING_PCM_8BIT, soundData.length,
-                AudioTrack.MODE_STATIC
-        );
-
-
-        track.write(soundData, 0, soundData.length);
-        track.play();
-        showTone();
-
-    }
-
-    public void stopSound(){
-        Log.d("Button", "Pausebutton clicked");
-        AudioTrack track = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate,
-                AudioFormat.CHANNEL_OUT_DEFAULT,
-                AudioFormat.ENCODING_PCM_8BIT, soundData.length,
-                AudioTrack.MODE_STATIC
-        );
-
-
-        track.write(soundData, 0, soundData.length);
-        track.flush();
-        track.stop();
-        track.release();
-
-
     }
 
     public void showTone(){
         tone.setVisibility(View.VISIBLE);
-        tone.setText(scale[currentIndex][1]);
+        tone.setText(toneGeneratorHelper.getScale()[currentScaleIndex][1]);
     }
 }
-
-
-
-
---
+++ app/src/main/java/com/example/steffen/theremin/ToneGeneratorHelper.java
@@ -0,0 +1,405 @@
+package com.example.steffen.theremin;
+
+import java.util.HashMap;
+
+public class ToneGeneratorHelper {
+
+    // Frequenzen der Töne
+    private static final String C = "262";
+    private static final String CIS = "277";
+    private static final String D = "294";
+    private static final String DIS = "311";
+    private static final String E = "330";
+    private static final String F = "349";
+    private static final String FIS = "370";
+    private static final String G = "392";
+    private static final String GIS = "415";
+    private static final String A = "440";
+    private static final String AIS = "466";
+    private static final String B = "494";
+    private static final String C2 = "523";
+    private static final String CIS2 = "554";
+    private static final String D2 = "587";
+    private static final String DIS2 = "622";
+    private static final String E2 = "659";
+    private static final String F2 = "698";
+    private static final String FIS2 = "740";
+    private static final String G2 = "784";
+    private static final String GIS2 = "831";
+    private static final String A2 = "880";
+    private static final String AIS2 = "932";
+    private static final String B2 = "988";
+
+    // C-Dur
+    private static final String C_DUR[][] = {
+            {C, "C"},
+            {D, "D"},
+            {E, "E"},
+            {F, "F"},
+            {G, "G"},
+            {A, "A"},
+            {B, "B"},
+            {C2, "C"}
+    };
+
+    // A-Moll
+    private static final String A_MOLL[][] = {
+            {A, "A"},
+            {B, "B"},
+            {C2, "C"},
+            {D2, "D"},
+            {E2, "E"},
+            {F2, "F"},
+            {G2, "G"},
+            {A2, "A"}
+    };
+
+    //#-Tonarten
+    // G-Dur
+    private static final String G_DUR[][] = {
+            {G, "G"},
+            {A, "A"},
+            {B, "B"},
+            {C2, "C"},
+            {D2, "D"},
+            {E2, "E"},
+            {FIS2, "F#"},
+            {G2, "G"}
+    };
+
+    // E-Moll
+    private static final String E_Moll[][] = {
+            {E, "E"},
+            {FIS, "F#"},
+            {G, "G"},
+            {A, "A"},
+            {B, "B"},
+            {C2, "C"},
+            {D2, "D"},
+            {E2, "E"}
+    };
+
+    // D-Dur
+    private static final String D_DUR[][] = {
+            {D, "D"},
+            {E, "E"},
+            {F, "F#"},
+            {G, "G"},
+            {A, "A"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {D2, "D"}
+    };
+
+    // B-Moll
+    private static final String B_MOLL[][] = {
+            {B, "B"},
+            {CIS2, "C#"},
+            {D2, "D"},
+            {E2, "E"},
+            {FIS2, "F#"},
+            {G2, "G"},
+            {A2, "A"},
+            {B2, "B"}
+    };
+
+    // A-Dur
+    private static final String A_DUR[][] = {
+            {A, "A"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {D2, "D"},
+            {E2, "E"},
+            {FIS2, "F#"},
+            {GIS2, "G#"},
+            {A2, "A"}
+    };
+
+    // F#-Moll
+    private static final String FIS_MOLL[][] = {
+            {FIS, "F#"},
+            {GIS, "G#"},
+            {A, "A"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {D2, "D"},
+            {E2, "E"},
+            {FIS2, "F#"}
+    };
+
+    // E-Dur
+    private static final String E_DUR[][] = {
+            {E, "E"},
+            {FIS, "F#"},
+            {GIS, "G#"},
+            {A, "A"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {DIS2, "D#"},
+            {E2, "E"}
+    };
+
+    // C#-Moll
+    private static final String CIS_MOLL[][] = {
+            {CIS, "C#"},
+            {DIS, "D#"},
+            {E, "E"},
+            {FIS, "F#"},
+            {GIS, "G#"},
+            {A, "A"},
+            {B, "B"},
+            {CIS2, "C#"}
+    };
+
+    // B-Dur
+    private static final String B_DUR[][] = {
+            {B, "B"},
+            {CIS2, "C#"},
+            {DIS2, "D#"},
+            {E2, "E"},
+            {FIS2, "F#"},
+            {GIS2, "G#"},
+            {AIS2, "A#"},
+            {B2, "B"}
+    };
+
+    // G#-Moll
+    private static final String GIS_MOLL[][] = {
+            {GIS, "G#"},
+            {AIS, "A#"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {DIS2, "D#"},
+            {E2, "E"},
+            {FIS2, "F#"},
+            {GIS2, "G#"}
+    };
+
+    // F#-Dur
+    private static final String FIS_DUR[][] = {
+            {FIS, "F#"},
+            {GIS, "G#"},
+            {AIS, "A#"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {DIS2, "D#"},
+            {F2, "F#"}, //enharmonische Verwechslung
+            {FIS2, "F#"}
+    };
+
+    // D#-Moll
+    private static final String DIS_MOLL[][] = {
+            {DIS, "D#"},
+            {F, "E#"}, //enharmonische Verwechslung
+            {FIS, "F#"},
+            {GIS, "G#"},
+            {AIS, "A#"},
+            {B, "B"},
+            {CIS2, "C#"},
+            {DIS2, "D#"}
+    };
+
+    // b-Tonarten
+    // F-Dur
+    private static final String F_DUR[][] = {
+            {F, "F"},
+            {G, "G"},
+            {A, "A"},
+            {AIS, "Bb"},
+            {C2, "C"},
+            {D2, "D"},
+            {E2, "E"},
+            {F2, "F"}
+    };
+
+    // D-Moll
+    private static final String D_MOLL[][] = {
+            {D, "D"},
+            {E, "E"},
+            {F, "F"},
+            {G, "G"},
+            {A, "A"},
+            {AIS, "Bb"},
+            {C2, "C"},
+            {D2, "D"}
+    };
+
+    // Bb-Dur
+    private static final String BB_DUR[][] = {
+            {AIS, "Bb"},
+            {C2, "C"},
+            {D2, "D"},
+            {DIS2, "Eb"},
+            {F2, "F"},
+            {G2, "G"},
+            {A2, "A"},
+            {AIS2, "Bb"}
+    };
+
+    // G-Moll
+    private static final String G_MOLL[][] = {
+            {G, "G"},
+            {A, "A"},
+            {AIS, "Bb"},
+            {C2, "C"},
+            {D2, "D"},
+            {DIS2, "Eb"},
+            {F2, "F"},
+            {G2, "G"}
+    };
+
+    // Eb-Dur
+    private static final String EB_DUR[][] = {
+            {DIS, "Eb"},
+            {F, "F"},
+            {G, "G"},
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {C2, "C"},
+            {D2, "D"},
+            {DIS2, "Eb"}
+    };
+
+    // C-Moll
+    private static final String C_MOLL[][] = {
+            {C, "C"},
+            {D, "D"},
+            {DIS, "Eb"},
+            {F, "F"},
+            {G, "G"},
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {C2, "C"}
+    };
+
+    // Ab-Dur
+    private static final String AB_DUR[][] = {
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {C2, "C"},
+            {CIS2, "Db"},
+            {DIS2, "Eb"},
+            {F2, "F"},
+            {G2, "G"},
+            {GIS2, "Ab"}
+    };
+
+    // F-Moll
+    private static final String F_MOLL[][] = {
+            {F, "F"},
+            {G, "G"},
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {C2, "C"},
+            {CIS2, "Db"},
+            {DIS2, "Eb"},
+            {F2, "F"}
+    };
+
+    // Db-Dur
+    private static final String DB_Dur[][] = {
+            {CIS, "Db"},
+            {DIS, "Eb"},
+            {F, "F"},
+            {FIS, "Gb"},
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {C, "C"},
+            {CIS2, "Db"}
+    };
+
+    // Bb-Moll
+    private static final String BB_MOLL[][] = {
+            {AIS, "Bb"},
+            {C2, "C"},
+            {CIS2, "Db"},
+            {DIS2, "Eb"},
+            {F2, "F"},
+            {FIS2, "Gb"},
+            {GIS2, "Ab"},
+            {AIS2, "Bb"}
+    };
+
+    // Gb-Dur
+    private static final String GB_DUR[][] = {
+            {FIS, "Gb"},
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {B, "Cb"}, //enharmonische Verwechslung
+            {CIS2, "Db"},
+            {DIS2, "Eb"},
+            {F2, "F"},
+            {FIS2, "Gb"}
+    };
+
+    // Eb-Moll
+    private static final String EB_MOLL[][] = {
+            {DIS, "Eb"},
+            {F, "F"},
+            {FIS, "Gb"},
+            {GIS, "Ab"},
+            {AIS, "Bb"},
+            {B, "Cb"}, //enharmonische Verwechslung
+            {CIS2, "Db"},
+            {DIS2, "Eb"}
+    };
+
+    private static final String defaultStartScale = "C_DUR";
+    private static final HashMap<String,String[][]> scaleMap = new HashMap<>();
+
+    private String scale[][];
+    private int sampleRate;
+
+    public ToneGeneratorHelper() {
+        scaleMap.put("C-Dur", C_DUR);
+        scaleMap.put("G-Dur", G_DUR);
+        scaleMap.put("D-Dur", D_DUR);
+        scaleMap.put("a-Dur", A_DUR);
+        scaleMap.put("E-Dur", E_DUR);
+        scaleMap.put("B-Dur", D_DUR);
+        scaleMap.put("F#-Dur", F_DUR);
+        scaleMap.put("Bb-Dur", FIS_DUR);
+        scaleMap.put("Eb-Dur", EB_DUR);
+        scaleMap.put("Ab-Dur", AB_DUR);
+        scaleMap.put("Db-Dur", DB_Dur);
+        scaleMap.put("Gb-Dur", GB_DUR);
+        scaleMap.put("A-Moll", A_MOLL);
+        scaleMap.put("E-Moll", E_Moll);
+        scaleMap.put("B-Moll", B_MOLL);
+        scaleMap.put("F#-Moll", FIS_MOLL);
+        scaleMap.put("C#-Moll", CIS_MOLL);
+        scaleMap.put("G#-Moll", GIS_MOLL);
+        scaleMap.put("D#-Moll", DIS_MOLL);
+        scaleMap.put("D-Moll", D_MOLL);
+        scaleMap.put("G-Moll", G_MOLL);
+        scaleMap.put("C-Moll", C_MOLL);
+        scaleMap.put("F-Moll", F_MOLL);
+        scaleMap.put("Bb-Moll", BB_MOLL);
+        scaleMap.put("Eb-Moll", EB_MOLL);
+
+        scale= scaleMap.get(defaultStartScale);
+        sampleRate = 44100;
+
+    }
+
+    public HashMap<String, String[][]> getScaleMap() {
+        return scaleMap;
+    }
+
+    public String[][] getScale() {
+        return scale;
+    }
+
+    public void setScale(String[][] scale) {
+        this.scale = scale;
+    }
+
+    public int getSampleRate() {
+        return sampleRate;
+    }
+
+    public void setSampleRate(int sampleRate) {
+        this.sampleRate = sampleRate;
+    }
+
+}
